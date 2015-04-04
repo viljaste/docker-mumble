91e9f6ca6172c74273558b0b683543a4e73be347
Jürgen Viljaste <j.viljaste@gmail.com>
2015-04-04 20:11:36 +0300

    Update

--- src/mumble/build/modules/mumble/manifests/mumble/supervisor.pp
+++ src/mumble/build/modules/mumble/manifests/mumble/supervisor.pp
@@ -1,4 +1,4 @@
-class jenkins::mumble::supervisor {
+class mumble::mumble::supervisor {
   file { '/etc/supervisor/conf.d/mumble.conf':
     ensure => present,
     source => 'puppet:///modules/mumble/etc/supervisor/conf.d/mumble.conf',
