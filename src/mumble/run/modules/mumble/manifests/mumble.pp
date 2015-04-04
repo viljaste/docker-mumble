ee223ada9372fc338bc73aff021196c09fbd6b84
Jürgen Viljaste <j.viljaste@gmail.com>
2015-04-04 20:05:24 +0300

    Update

--- src/mumble/run/modules/mumble/manifests/mumble.pp
+++ src/mumble/run/modules/mumble/manifests/mumble.pp
@@ -1,6 +1,10 @@
 class mumble::mumble {
   bash_exec { 'mkdir -p /mumble/data': }
 
+  bash_exec { 'chown -R mumble-server.mumble-server /mumble/data':
+    require => Bash_exec['mkdir -p /mumble/data']
+  }
+
   file { '/etc/mumble-server.ini':
     ensure => present,
     content => template('mumble/mumble-server.ini.erb'),
